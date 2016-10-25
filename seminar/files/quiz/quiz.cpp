da3c372cf3e643f9e3224307747c3c9897bb678d
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2016-10-25 22:36:05 +0300

    Add requested changes for review for the quiz task

--- seminar/files/quiz/quiz.cpp
+++ seminar/files/quiz/quiz.cpp
@@ -12,218 +12,231 @@ using std::ofstream;
 using std::ios;
 
 struct Result {
-    char name[32];
-    int questions[20];
+  char name[32];
+  int questions[20];
 
-    Result() {
-        for (int i = 0; i < 20; ++i) {
-            questions[i] = 0;
-        }
+  Result() {
+    for (int i = 0; i < 20; ++i) {
+      questions[i] = 0;
+    }
+  }
+
+  int total() {
+    int sum = 0;
+
+    for (int i = 0; i < 20; ++i) {
+      sum += questions[i];
     }
 
-    int total() {
-        int sum = 0;
+    return sum;
+  }
 
-        for (int i = 0; i < 20; ++i) {
-            sum += questions[i];
-        }
-
-        return sum;
+  Result &operator+=(const Result &other) {
+    for (int i = 0; i < 20; ++i) {
+      questions[i] += other.questions[i];
     }
 
-    Result& operator+=(const Result& other) {
-        for (int i = 0; i < 20; ++i) {
-            questions[i] += other.questions[i];
-        }
+    return *this;
+  }
 
-        return *this;
+  Result &operator-=(const Result &other) {
+    for (int i = 0; i < 20; ++i) {
+      questions[i] -= other.questions[i];
     }
 
-    Result& operator-=(const Result& other) {
-        for (int i = 0; i < 20; ++i) {
-            questions[i] -= other.questions[i];
-        }
+    return *this;
+  }
 
-        return *this;
+  Result &operator/=(const double n) {
+    for (int i = 0; i < 20; ++i) {
+      questions[i] /= n;
     }
 
-    Result& operator/=(const double n) {
-        for (int i = 0; i < 20; ++i) {
-            questions[i] /= n;
-        }
+    return *this;
+  }
 
-        return *this;
+  Result &square() {
+    for (int i = 0; i < 20; ++i) {
+      questions[i] *= questions[i];
     }
 
-    Result& square() {
-        for (int i = 0; i < 20; ++i) {
-            questions[i] *= questions[i];
-        }
+    return *this;
+  }
 
-        return *this;
+  Result &squareRoot() {
+    for (int i = 0; i < 20; ++i) {
+      questions[i] = sqrt(questions[i]);
     }
 
-    Result& squareRoot() {
-        for (int i = 0; i < 20; ++i) {
-            questions[i] = sqrt(questions[i]);
-        }
-
-        return *this;
-    }
+    return *this;
+  }
 };
 
-istream& operator>>(istream &in, Result &result) {
-    in >> result.name;
-    for (int i = 0; i < 20; i++) {
-        in >> result.questions[i];
-    }
+istream &operator>>(istream &in, Result &result) {
+  in >> result.name;
+  for (int i = 0; i < 20; i++) {
+    in >> result.questions[i];
+  }
 }
 
-ostream& operator<<(ostream &out, const Result &result) {
-    out << result.name << "\n";
-    for (int i = 0; i < 20; i++) {
-        out << result.questions[i] << " ";
-    }
-    out << "\n";
+ostream &operator<<(ostream &out, const Result &result) {
+  out << result.name << "\n";
+  for (int i = 0; i < 20; i++) {
+    out << result.questions[i] << " ";
+  }
+  out << "\n";
 }
 
 class QuizSaver {
-    ofstream file;
+  ofstream file;
 public:
-    QuizSaver(const char* filename) {
-        file.open(filename, ios::app);
-    }
+  QuizSaver(const char *filename) {
+    file.open(filename, ios::app);
+  }
 
-    ~QuizSaver() {
-        file.close();
-    }
+  ~QuizSaver() {
+    file.close();
+  }
 
-    void addResult() {
-        Result result;
-        cin >> result;
-        file << result;
+  void addResult() {
+    Result result;
+    cin >> result;
+    file << result;
 
-        // force writing to the file
-        // for proper calculations when reading immediately
-        file.flush();
-    }
+    // force writing to the file
+    // for proper calculations when reading immediately
+    file.flush();
+  }
 };
 
 class QuizReader {
-    ifstream file;
+  ifstream file;
 
-    void goToBeginningOfFile() {
-        // go to beginning of file
-        file.clear();
-        file.seekg(0, ios::beg);
+  void goToBeginningOfFile() {
+    // go to beginning of file
+    file.clear();
+    file.seekg(0, ios::beg);
+  }
+
+  Result averageByQuestion() {
+    Result total, tempResult;
+    int count = 0;
+
+    goToBeginningOfFile();
+
+    while (file >> tempResult) {
+      ++count;
+
+      total += tempResult;
     }
 
-    Result averageByQuestion() {
-        Result total, tempResult;
-        int count = 0;
-
-        goToBeginningOfFile();
-
-        while (file >> tempResult) {
-            ++count;
-
-            total += tempResult;
-        }
-
-        return total /= count;
+    if (count == 0) {
+      // return zeroes if not enough results
+      return total;
     }
+    else {
+      return total /= count;
+    }
+  }
+
 public:
-    QuizReader(const char* filename) {
-        file.open(filename);
+  QuizReader(const char *filename) {
+    file.open(filename);
+  }
+
+  ~QuizReader() {
+    file.close();
+  }
+
+  Result bestResult() {
+    Result bestResult, tempResult;
+
+    goToBeginningOfFile();
+
+    file >> bestResult;
+
+    while (file >> tempResult) {
+      if (tempResult.total() > bestResult.total()) {
+        bestResult = tempResult;
+      }
     }
 
-    ~QuizReader() {
-        file.close();
-    }
+    return bestResult;
+  }
 
-    Result bestResult() {
-        Result bestResult, tempResult;
+  Result bestCombinedResult() {
+    Result bestResult, tempResult;
 
-        goToBeginningOfFile();
+    goToBeginningOfFile();
 
-        file >> bestResult;
+    file >> bestResult;
+    strcpy(bestResult.name, "Best combined");
 
-        while (file >> tempResult) {
-            if (tempResult.total() > bestResult.total()) {
-                bestResult = tempResult;
-            }
+    while (file >> tempResult) {
+      for (int i = 0; i < 20; ++i) {
+        if (tempResult.questions[i] > bestResult.questions[i]) {
+          bestResult.questions[i] = tempResult.questions[i];
         }
-
-        return bestResult;
+      }
     }
 
-    Result bestCombinedResult() {
-        Result bestResult, tempResult;
+    return bestResult;
+  }
 
-        goToBeginningOfFile();
+  Result standardDeviation() {
+    Result average = averageByQuestion();
+    Result sum, tempResult;
+    int count = -1;
 
-        file >> bestResult;
-        strcpy(bestResult.name, "Best combined");
+    goToBeginningOfFile();
 
-        while (file >> tempResult) {
-            for (int i = 0; i < 20; ++i) {
-                if (tempResult.questions[i] > bestResult.questions[i]) {
-                    bestResult.questions[i] = tempResult.questions[i];
-                }
-            }
-        }
-
-        return bestResult;
+    while (file >> tempResult) {
+      ++count;
+      sum += (tempResult -= average).square();
     }
 
-    Result standardDeviation() {
-        Result average = averageByQuestion();
-        Result sum, tempResult;
-        int count = -1;
+    strcpy(sum.name, "Standard deviation");
 
-        goToBeginningOfFile();
-
-        while (file >> tempResult) {
-            ++count;
-            sum += (tempResult -= average).square();
-        }
-
-        strcpy(sum.name, "Standard deviation");
-        return (sum /= count).squareRoot();
+    if (count > 1) {
+      return (sum /= count).squareRoot();
     }
+    else {
+      // return zeroes if not enough results
+      return sum;
+    }
+  }
 };
 
 int main() {
+  QuizSaver quizSaver("results.txt");
+  QuizReader quizReader("results.txt");
 
-    QuizSaver quizSaver("results.txt");
-    QuizReader quizReader("results.txt");
-
-    char choice;
-    cout << "Enter choice: ";
-    while (cin >> choice) {
-        switch (choice) {
-            case 'i':
-                cout << "Enter a name and 20 results: \n";
-                quizSaver.addResult();
-                break;
-            case 'b':
-                cout << "The best result is: \n";
-                cout << quizReader.bestResult();
-                break;
-            case 'm':
-                cout << quizReader.bestCombinedResult();
-                break;
-            case 's':
-                cout << quizReader.standardDeviation();
-                break;
-            case 'e':
-                // exit;
-                return 0;
-            default:
-                cout << "Not a valid choice. Try again!\n";
-        }
-        cout << "Enter choice: ";
+  char choice;
+  cout << "Enter choice: ";
+  while (cin >> choice) {
+    switch (choice) {
+      case 'i':
+        cout << "Enter a name and 20 results:\n";
+        quizSaver.addResult();
+        break;
+      case 'b':
+        cout << "The best result is:\n";
+        cout << quizReader.bestResult();
+        break;
+      case 'm':
+        cout << quizReader.bestCombinedResult();
+        break;
+      case 's':
+        cout << quizReader.standardDeviation();
+        break;
+      case 'e':
+        // exit
+        return 0;
+      default:
+        cout << "Not a valid choice. Try again!\n";
     }
+    cout << "Enter choice: ";
+  }
 
-    return 0;
+  return 0;
 }
