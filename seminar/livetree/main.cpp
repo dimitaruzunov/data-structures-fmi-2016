8306add79829976eaf1ae3fc74778e5abc421c20
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2016-12-14 14:28:50 +0200

    Added bst implementation

--- seminar/livetree/main.cpp
+++ seminar/livetree/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
+using std::ostream;
+using std::cout;
+using std::endl;
+using std::vector;
 
 struct Node {
   int data;
@@ -8,11 +12,13 @@ struct Node {
   Node* left;
   Node* right;
 
+  // changed: added data(data)
   Node(int data, Node* left = nullptr, Node* right = nullptr)
     : data(data), left(left), right(right) {}
 };
 
 class BinaryTree {
+protected:
   Node* root;
 
   BinaryTree(Node* node) {
@@ -39,6 +45,7 @@ class BinaryTree {
     Node* toDel = to;
     to = from;
     from = nullptr;
+    // changed: eraseNode(to)->eraseNode(toDel)
     eraseNode(toDel);
   }
 
@@ -86,19 +93,137 @@ public:
     if (bt.empty())
       return os << '.';
 
+    // changed: added braces
     return os << '(' << bt.getRoot() << ' ' << bt.leftTree() << ' '
       << bt.rightTree() << ')';
   }
 
 };
 
+class OrderedTree : public BinaryTree {
+  Node*& findNode(const int& data) {
+    Node** p = &root;
+    while (*p != nullptr && (*p)->data != data) {
+      if (data < (*p)->data)
+        p = &((*p)->left);
+      else
+        p = &((*p)->right);
+    }
+    return *p;
+  }
+
+  Node*& findMin(Node*& node) {
+    Node** p = &node;
+    while ((*p)->left != nullptr) {
+      p = &((*p)->left);
+    }
+    return *p;
+  }
+public:
+  int* search(const int& data) {
+    Node*& found = findNode(data);
+    if (found == nullptr)
+      return nullptr;
+    return &found->data;
+  }
+
+  bool addElement(const int& data) {
+    Node*& here = findNode(data);
+    if (here != nullptr)
+      return false;
+
+    here = new Node(data);
+    return true;
+  }
+
+  bool deleteElement(const int& data) {
+    Node*& p = findNode(data);
+    if (p == nullptr)
+      return false;
+    if (p->left == nullptr) {
+      Node* toDel = p;
+      p = p->right;
+      delete toDel;
+      return true;
+    }
+    if (p->right == nullptr) {
+      Node* toDel = p;
+      p = p->left;
+      delete toDel;
+      return true;
+    }
+
+    Node*& m = findMin(p->right);
+    p->data = m->data;
+
+    Node* toDel = m;
+    m = m->right;
+    delete toDel;
+
+    return true;
+  }
+};
+
+void printInOrder(const BinaryTree& tree) {
+  if (!tree.empty()) {
+    printInOrder(tree.leftTree());
+    cout << tree.getRoot() << " ";
+    printInOrder(tree.rightTree());
+  }
+}
+
+void printInOrderOnlyNs(const BinaryTree& tree, int n
+    , int& current) {
+  if (!tree.empty()) {
+    printInOrderOnlyNs(tree.leftTree(), n, current);
+    if (current % n == 0)
+      cout << tree.getRoot() << " ";
+    ++current;
+    printInOrderOnlyNs(tree.rightTree(), n, current);
+  }
+}
+
+void printInOrderNs(const BinaryTree& tree, int n) {
+  int current = 0;
+  printInOrderOnlyNs(tree, n, current);
+}
+
+void toVectorInOrder(const BinaryTree& tree, vector<int>& v) {
+  if (!tree.empty()) {
+    toVectorInOrder(tree.leftTree(), v);
+    v.push_back(tree.getRoot());
+    toVectorInOrder(tree.rightTree(), v);
+  }
+}
+
+vector<int> sortUsingOT(vector<int>& list) {
+  OrderedTree t;
+  for (int el : list)
+    t.addElement(el);
+  vector<int> v;
+  toVectorInOrder(t, v);
+  return v;
+}
+
 int main() {
   BinaryTree bt(3);
   BinaryTree bt2(3, 4, 5);
 
-  BinaryTree bt3(3, 4, BinaryTree(5, 6, 7));
+//  OrderedTree bt3(3, 2, OrderedTree(6, 5, 7));
+OrderedTree t;
+  t.addElement(4);
+  t.addElement(5);
+  t.addElement(1);
+  t.addElement(2);
+  t.addElement(8);
+  t.addElement(7);
+  t.addElement(-1);
 
-  cout << bt3 << endl;
+  cout << t << endl;
+  printInOrder(t);
+  cout << endl;
+
+  printInOrderNs(t, 3);
 
   return 0;
 }
