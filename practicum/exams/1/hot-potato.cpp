78619c584b1a931be89d685b50e7a034d8989ad3
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2016-12-26 18:14:45 +0200

    Add solution to first exam

---
+++ practicum/exams/1/hot-potato.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <string>
+#include <queue>
+
+using std::cout;
+using std::ifstream;
+using std::ofstream;
+using std::list;
+using std::string;
+using std::queue;
+
+list<string> hot_potato(const list<string>& players, int passes) {
+  queue<string> circle;
+  list<string> players_ranking;
+
+  for (const string& player : players) {
+    circle.push(player);
+  }
+
+  while (circle.size() > 1) {
+    for (int i = 0; i < passes; ++i) {
+      const string& current_player = circle.front();
+      circle.pop();
+      circle.push(current_player);
+    }
+
+    players_ranking.push_back(circle.front());
+    cout << circle.front() << '\n';
+    circle.pop();
+  }
+
+  players_ranking.push_back(circle.front());
+  cout << circle.front() << '\n';
+  circle.pop();
+
+  return players_ranking;
+}
+
+list<string> read_game(const char* game_input_file, int& passes) {
+  ifstream file(game_input_file);
+
+  int number_of_players;
+  file >> number_of_players;
+
+  list<string> players;
+
+  for (int i = 0; i < number_of_players; ++i) {
+    string current_player;
+    file >> current_player;
+    players.push_back(current_player);
+  }
+
+  file >> passes;
+
+  file.close();
+
+  return players;
+}
+
+void write_game(const list<string>& players_ranking, const char* game_output_file) {
+  ofstream file(game_output_file);
+
+  for (const string& player : players_ranking) {
+    file << player << '\n';
+  }
+
+  file.close();
+}
+
+int main() {
+  int passes;
+  list<string> players = read_game("game-input.txt", passes);
+
+  list<string> players_ranking = hot_potato(players, passes);
+
+  write_game(players_ranking, "game-output.txt");
+
+  return 0;
+}
